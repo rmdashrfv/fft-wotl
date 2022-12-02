9028e05b78cf35607ff59fff1edfe53c922a72f5
Michael Law <rmdashrfv@hey.com>
2022-12-02 12:13:05 -0500

    added two of the many genders

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ export const UNITS = [
     ct: 0,
     name: null, // touch on this
     job: "Squire",
+    gender: "male",
     attack: 10,
     move: 3,
     speed: 5,
@@ -17,7 +18,30 @@ export const UNITS = [
     },
     level: 1,
     exp: 0,
-    imgSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job59.gif",
+    imgSrc: "http://www.videogamesprites.net/FinalFantasyTactics/Jobs/Squire/Squire1M-SW.gif",
+    portraitSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job60.gif",
+    cost: 1000
+  },
+  {
+    hp: 100,
+    mp: 10,
+    ct: 0,
+    name: null, // touch on this
+    job: "Squire",
+    gender: "female",
+    attack: 10,
+    move: 3,
+    speed: 5,
+    equipment: {
+      rHand: null,
+      lHand: null,
+      helmet: null,
+      armor: null,
+      accessory: null
+    },
+    level: 1,
+    exp: 0,
+    imgSrc: "http://www.videogamesprites.net/FinalFantasyTactics/Jobs/Squire/Squire1F-SW.gif",
     portraitSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job60.gif",
     cost: 1000
   },
@@ -27,6 +51,7 @@ export const UNITS = [
     ct: 0,
     name: null, // touch on this
     job: "Chemist",
+    gender: "male",
     attack: 5,
     move: 3,
     speed: 4,
@@ -39,7 +64,30 @@ export const UNITS = [
     },
     level: 1,
     exp: 0,
-    imgSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job13.gif",
+    imgSrc: "http://www.videogamesprites.net/FinalFantasyTactics/Jobs/Chemist/Chemist1M-SW.gif",
+    portraitSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job14.gif",
+    cost: 800
+  },
+  {
+    hp: 75,
+    mp: 5,
+    ct: 0,
+    name: null, // touch on this
+    job: "Chemist",
+    gender: "female",
+    attack: 5,
+    move: 3,
+    speed: 4,
+    equipment: {
+      rHand: null,
+      lHand: null,
+      helmet: null,
+      armor: null,
+      accessory: null
+    },
+    level: 1,
+    exp: 0,
+    imgSrc: "http://www.videogamesprites.net/FinalFantasyTactics/Jobs/Chemist/Chemist1F-SW.gif",
     portraitSrc: "https://fantasyanime.com/finalfantasy/fftactics/images/fftactics_job14.gif",
     cost: 800
   }
